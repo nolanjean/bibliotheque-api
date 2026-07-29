6bcfee0958d1506029183ad05ed4f856b602959e
nolanjean <nobodyplayeur@gmail.com>
2026-07-29 09:47:01 +0200

    test: ajout du test LivreService.supprimer avec livre encore emprunte

---
+++ src/test/java/com/bibliotheque/bibliotheque_api/service/LivreServiceTest.java
@@ -0,0 +1,56 @@
+package com.bibliotheque.bibliotheque_api.service;
+
+import com.bibliotheque.bibliotheque_api.entity.Emprunt;
+import com.bibliotheque.bibliotheque_api.entity.Livre;
+import com.bibliotheque.bibliotheque_api.enums.StatutEmprunt;
+import com.bibliotheque.bibliotheque_api.exception.LivreNonRenduException;
+import com.bibliotheque.bibliotheque_api.repository.AuteurRepository;
+import com.bibliotheque.bibliotheque_api.repository.EmpruntRepository;
+import com.bibliotheque.bibliotheque_api.repository.LivreRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class LivreServiceTest {
+
+    private LivreRepository livreRepository;
+    private EmpruntRepository empruntRepository;
+    private AuteurRepository auteurRepository;
+    private LivreService livreService;
+
+    @BeforeEach
+    void setUp(){
+        livreRepository = mock(LivreRepository.class);
+        empruntRepository = mock(EmpruntRepository.class);
+        auteurRepository = mock(AuteurRepository.class);
+        livreService = new LivreService(livreRepository,empruntRepository,auteurRepository);
+    }
+
+    @Test
+    void livreSupprimer_devraitLeverException_SiStatutRendu(){
+        //Arange
+        Livre livre = new Livre();
+        livre.setId(1L);
+
+        Emprunt emprunt = new Emprunt();
+        emprunt.setId(1L);
+        emprunt.setLivre(livre);
+        emprunt.setStatut(StatutEmprunt.EN_COURS);
+
+        //when ??
+        when(livreRepository.findById(1L)).thenReturn(Optional.of(livre));
+        when(empruntRepository.findByLivreIdAndStatut(1L, StatutEmprunt.EN_COURS)).thenReturn(List.of(emprunt));
+
+        assertThrows(LivreNonRenduException.class, () -> {
+            livreService.supprimer(1L);
+        });
+
+
+    }
+}
