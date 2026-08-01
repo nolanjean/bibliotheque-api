0c1763be94257447bdc3eea4d4c5d0e75851ee96
nolanjean <nobodyplayeur@gmail.com>
2026-08-01 10:52:40 +0200

    test: ajout du test AuteurService.supprimer avec livres encore lies

---
+++ src/test/java/com/bibliotheque/bibliotheque_api/service/AuteurServiceTest.java
@@ -0,0 +1,42 @@
+package com.bibliotheque.bibliotheque_api.service;
+
+import com.bibliotheque.bibliotheque_api.entity.Auteur;
+import com.bibliotheque.bibliotheque_api.entity.Livre;
+import com.bibliotheque.bibliotheque_api.exception.AuteurPossedeLivresException;
+import com.bibliotheque.bibliotheque_api.repository.AuteurRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class AuteurServiceTest {
+
+    private AuteurRepository auteurRepository;
+    private AuteurService auteurService;
+
+    @BeforeEach
+    void setUp(){
+        auteurRepository = mock(AuteurRepository.class);
+        auteurService = new AuteurService(auteurRepository);
+    }
+
+    @Test
+    void supprimerAuteur_devraitLeverExceptionSiEncoreLivre(){
+        //Arrange
+        Auteur auteur = new Auteur();
+        auteur.setId(1L);
+        Livre livre = new Livre();
+        auteur.setLivres(List.of(livre));
+
+        when(auteurRepository.findById(1L)).thenReturn(Optional.of(auteur));
+
+        assertThrows(AuteurPossedeLivresException.class, () -> auteurService.supprimer(1L));
+
+    }
+
+}
