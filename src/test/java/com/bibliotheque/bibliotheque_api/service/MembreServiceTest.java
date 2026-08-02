afb7d4abf7cefe2a67a64e4a063e0e3b8a22ada5
nolanjean <nobodyplayeur@gmail.com>
2026-08-02 13:34:58 +0200

    test: ajout du test MembreService.supprimer avec emprunts en cours

--- src/test/java/com/bibliotheque/bibliotheque_api/service/MembreServiceTest.java
+++ src/test/java/com/bibliotheque/bibliotheque_api/service/MembreServiceTest.java
@@ -1,15 +1,20 @@
 package com.bibliotheque.bibliotheque_api.service;
 
 import com.bibliotheque.bibliotheque_api.dto.request.RegisterRequest;
+import com.bibliotheque.bibliotheque_api.entity.Emprunt;
+import com.bibliotheque.bibliotheque_api.entity.Livre;
 import com.bibliotheque.bibliotheque_api.entity.Membre;
 import com.bibliotheque.bibliotheque_api.enums.Role;
+import com.bibliotheque.bibliotheque_api.enums.StatutEmprunt;
 import com.bibliotheque.bibliotheque_api.exception.EmailDejaUtiliseException;
+import com.bibliotheque.bibliotheque_api.exception.MembrePossedeEmpruntsException;
 import com.bibliotheque.bibliotheque_api.repository.EmpruntRepository;
 import com.bibliotheque.bibliotheque_api.repository.MembreRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -60,6 +65,22 @@ public class MembreServiceTest {
         assertEquals("jean@email.com", resultat.getEmail());
         assertEquals("hashSimule123", resultat.getMotDePasse());
         assertEquals(Role.MEMBRE, resultat.getRole());
+    }
 
+    @Test
+    void supprimerMembre_DevraitLeverException_LivreEmprunte(){
+        Emprunt emprunt = new Emprunt();
+        emprunt.setStatut(StatutEmprunt.EN_COURS);
+
+        Membre membre = new Membre();
+        membre.setId(1L);
+        membre.setEmprunts(List.of(emprunt));
+
+        when(membreRepository.findById(1L)).thenReturn(Optional.of(membre));
+        when(empruntRepository.findByMembreIdAndStatut(1L,StatutEmprunt.EN_COURS)).thenReturn(List.of(emprunt));
+
+        assertThrows(MembrePossedeEmpruntsException.class, () -> {
+            membreService.supprimer(1L);
+        });
     }
 }
