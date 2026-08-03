a4e52a6643c8309c72aa5dd89b1acd02fce40a47
nolanjean <nobodyplayeur@gmail.com>
2026-08-03 14:57:53 +0200

    feat: annotation de validation sur RegisterRequest

--- src/main/java/com/bibliotheque/bibliotheque_api/dto/request/RegisterRequest.java
+++ src/main/java/com/bibliotheque/bibliotheque_api/dto/request/RegisterRequest.java
@@ -1,8 +1,19 @@
 package com.bibliotheque.bibliotheque_api.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RegisterRequest(
+        @NotBlank(message = "Le nom est obligatoire")
         String nom,
+
+        @NotBlank(message = "L'email est obligatoire")
+        @Email(message = "Email invalide")
         String email,
+
+        @NotBlank(message = "Le mot de pass est obligatoire")
+        @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
         String motDePasse
 ) {
 }
